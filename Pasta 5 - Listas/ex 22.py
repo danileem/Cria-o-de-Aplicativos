6febdc56197ce610078f6e9b55c72cb10201bd17
danileem <daniellemos.joi@gmail.com>
2026-09-23 20:30:04 -0300

    Add files via upload

---
+++ Pasta 5 - Listas/ex 22.py
@@ -0,0 +1,13 @@
+Vnumeros = (1, 2, 3, 4, 5 ,6 ,7 ,8, 10, 11, 12)
+
+for i in Vnumeros:
+    print(Vnumeros)
+
+somaT = 0
+
+for i in Vnumeros:   
+    print(i)
+
+    somaT += i
+
+print("Todos os números somados são: ",somaT)
